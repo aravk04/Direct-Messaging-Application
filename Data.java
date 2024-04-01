eb9f473656c2078a2b28da630b8fa7fe87ea2f7e
zonglin133 <102833970+zonglin133@users.noreply.github.com>
2024-04-01 17:45:42 -0400

    Add files via upload

--- Data.java
+++ Data.java
@@ -1,13 +1,28 @@
 import java.util.ArrayList;
 
+/**
+ * An interface of data
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Harshil, Zonglin
+ * @version April 1st, 2024
+ */
 public interface Data {
     ArrayList<User> getUsers();
+
     void setUsers(ArrayList<User> users);
+
     void readFile() throws IncorrectInfoException;
+
     //void writeFile();
     boolean searchUser(String username);
+
     boolean addUser(String info);
+
     String viewUser(String username);
+
     boolean editUser(String oldInfo, String newInfo) throws BadInputException;
+
     boolean removeUser(String username);
 }
