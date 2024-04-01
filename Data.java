d9cee2d89bfcb8ef3ad088fb85aeab0a0385d600
Harshil Shah <shahharshilc@gmail.com>
2024-03-31 21:07:48 -0400

    Finalized the Database.java, added remaining methods, and implemented file reading and writing.

--- Data.java
+++ Data.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 
 public interface Data {
+    ArrayList<User> getUsers();
+    void setUsers(ArrayList<User> users);
     void readFile() throws IncorrectInfoException;
     void writeFile();
     boolean searchUser(String username);
     boolean addUser(String info);
     String viewUser(String username);
     boolean editUser(String oldInfo, String newInfo);
+    boolean removeUser(String username);
 }
