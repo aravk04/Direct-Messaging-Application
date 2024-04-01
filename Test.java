3722cf3bfccdb5c09264e6333e931f03cd2bdfc0
efaruqi <eeshafaruqi@gmail.com>
2024-04-01 00:10:04 -0400

    Updated test.java (practice commits)

--- Test.java
+++ Test.java
@@ -6,7 +6,7 @@ public class Test {
     public static void main(String[] args) throws FileNotFoundException, IOException, BadInputException {
        User e = new User("Eesha Faruqi", "efaruqi", "00003062", "efaruqi@gmail.com"); 
        User e1 = new User("Eesha1 Faruqi", "efaruqi1", "00003062", "efaruqi1@gmail.com"); 
-       Message m = new Message(e, new ArrayList<>(List.of(e1)), "hello eesha1 this is at 12");
+       Message m = new Message(e, new ArrayList<>(List.of(e1)), "hello eesha1");
        e.sendMessage(m);
     }
 }
