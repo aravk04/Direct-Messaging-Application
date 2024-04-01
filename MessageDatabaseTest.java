f50498d5e4796da5c3dc7dcae9e4144214fbe3c6
efaruqi <eeshafaruqi@gmail.com>
2024-04-01 16:55:02 -0400

    Update MessageDatabaseTest.java

--- MessageDatabaseTest.java
+++ MessageDatabaseTest.java
@@ -1,13 +1,15 @@
 /**
- * MessageDatabaseTest
- *
- * Run tests for MessageDatabase class
- *
- * @author Eesha Faruqi, Arav Kolli, Zonglin Jia,
- * Harshil Shah, Benjamin Ascano
- * @version March 31, 2024
- */
+* MessageDatabaseTest
+*
+* Run tests for MessageDatabase class
+*
+* @author Eesha Faruqi, Arav Kolli, Zonglin Jia,
+* Harshil Shah, Benjamin Ascano
+* @version March 31, 2024
+*/
 import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.*;
 import java.util.ArrayList;
@@ -15,6 +17,16 @@ import java.util.ArrayList;
 public class MessageDatabaseTest {
 
     @Test
+    private String fileName = "efaruqi,efaruqi1.csv";
+
+    public void setUp() throws IOException {
+        // Create the file if it doesn't exist
+        File file = new File(fileName);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+    }
+
     public void testAddMessage() {
         User sender = new User("Eesha Faruqi", "efaruqi", "00003062", "efaruqi@gmail.com");
         User receiver = new User("Eesha1 Faruqi", "efaruqi1", "00003063", "efaruqi1@gmail.com");
@@ -25,7 +37,6 @@ public class MessageDatabaseTest {
 
         MessageDatabase database = new MessageDatabase();
 
-        String fileName = "test_messages.txt";
         try {
             assertTrue(database.addMessage(fileName, message));
 
@@ -40,10 +51,14 @@ public class MessageDatabaseTest {
             }
             reader.close();
             assertTrue("Message not found in the file", found);
+        } catch (FileNotFoundException e) {
+            fail("File not found: " + e.getMessage());
         } catch (IOException e) {
             fail("IOException occurred: " + e.getMessage());
         }
+    }
 
+    public void cleanup() {
         // Clean up test file
         File file = new File(fileName);
         if (file.exists()) {
