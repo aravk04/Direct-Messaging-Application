ed5cd5270851de048f926e0d2eaa0f08a9ce5a08
bascano <164688288+bascano@users.noreply.github.com>
2024-04-13 17:49:58 -0400

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,65 +1,124 @@
-import java.time.*;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.io.*;
+import java.io.File;
+import java.util.Collections;
 /**
- * Message
+ * User
  *
- * Used for users to communicate includes things
- * like sender, receiver, time, etc.
+ * Represents user and contains information
+ * about them
  *
  * @author Eesha Faruqi, Arav Kolli, Zonglin Jia,
  * Harshil Shah, Benjamin Ascano
  * @version March 31, 2024
  */
+public class User extends Thread implements UserInterface {
+    // Variables
+    private String realName;
+    private ArrayList<User> friends;
+    private ArrayList<User> blocked;
+    private String password;
+    private String emailAddress;
+    private String userName;
 
-public class Message implements MessageInterface {
-    private User sender;
-    private ArrayList<User> receivers;
-    private String content;
-    private String timestamp;
-    private long exactTime;
+    // Constructor
+    public User(String realName, String userName, String password, String emailAddress) throws BadInputException {
+        // Validate username
+        if (userName == null || userName.isEmpty()) {
+            throw new BadInputException("Username cannot be empty");
+        }
+        //check password
+        if (password == null || password.length() < 8) {
+            throw new BadInputException("Password must be at least 8 characters long");
+        }
 
-    public Message(User sender, ArrayList<User> receivers, String content) {
-        this.sender = sender;
-        this.receivers = receivers;
-        this.content = content;
-        this.timestamp = LocalDate.now().format(DateTimeFormatter.ofPattern("dd LLLL yyyy")) + " "
-                + LocalTime.now().format(DateTimeFormatter.ofPattern("HH[:mm]"));
-        this.exactTime = Instant.now().toEpochMilli();
+        this.realName = realName;
+        this.userName = userName;
+        this.password = password;
+        this.emailAddress = emailAddress;
+        this.friends = new ArrayList<User>();
+        this.blocked = new ArrayList<User>();
     }
+    
+    public void sendMessage(ArrayList<User> receivers, String content) throws FileNotFoundException, IOException {
+        Message m = new Message(this, receivers, content);
+        
+        ArrayList<String> usernames = new ArrayList<>();
+        String fileName = "";
+        usernames.add(m.getSender().getUsername());
+        for (User u : m.getReceivers()) {
+            usernames.add(u.getUsername());
+        }
+        Collections.sort(usernames);
+        for (String username : usernames) {
+            fileName += username + ",";
+        }
+        fileName = fileName.substring(0, fileName.length() - 1) + ".csv";
+        File file = new File(fileName);
+        if (!file.exists()) {
+            file.createNewFile();
+            MessageDatabase db = new MessageDatabase();
+            db.addMessage(fileName, m);
+        } else {
+            MessageDatabase db = new MessageDatabase();
+            db.addMessage(fileName, m);
+        }
 
-    // Getters
-    public User getSender() {
-        return sender;
     }
+    // Add friend to list
 
-    public ArrayList<User> getReceivers() {
-        return receivers;
-    }
+    public boolean addFriend(User u) {
 
-    public String getContent() {
-        return content;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public long getExactTime() {
-        return exactTime;
-    }
-
-    @Override
-    public boolean sameDM(MessageInterface m) {
+        if ((u != null && !friends.contains(u)) && (!blocked.contains(u))) {
+            friends.add(u);
+            return true;
+        }
         return false;
     }
 
-    public boolean sameDM(Message m) {
-        for (User receiver : m.getReceivers()) {
-            if (receiver.equals(this.getSender()) || this.receivers.contains(receiver)) {
-                for (User thisReceiver : this.receivers) {
-                    if (thisReceiver.equals(m.getSender()) || m.getReceivers().contains(thisReceiver)) {
-                        return true;
+    // Block a user
+    public boolean blockUser(User u) {
+
+
+        if ((u != null && !blocked.contains(u))) {
+            blocked.add(u);
+            this.removeFriend(u);
+            return true;
+        }
+        return false;
+    }
+    // unblock a user
+    public boolean unblockUser(User u) {
+        if (u != null && blocked.contains(u)) {
+            blocked.remove(u);
+            return true;
+        }
+        return false;
+    }
+
+
+    // Remove friend from list
+    public boolean removeFriend(User u) {
+        for (int i = 0; i < friends.size(); i++) {
+            if (friends.get(i).equals(u)) {
+                friends.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //check if they have the same username
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof User) {
+            User u = (User) o;
+            if (u.retrieveName().equals(this.retrieveName())) {
+                if (u.getEmailAddress().equals(this.getEmailAddress())) {
+                    if (u.getUsername().equals(this.getUsername())) {
+                        if (u.getPassword().equals(this.getPassword())) {
+                            return true;
+                        }
                     }
                 }
             }
@@ -67,22 +126,63 @@ public class Message implements MessageInterface {
         return false;
     }
 
+    // Getters
+
+    public ArrayList<User> getFriends() {
+        return new ArrayList<User>(friends);
+    }
+
+    public ArrayList<User> getBlocked() {
+        return new ArrayList<User>(blocked);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+    public String retrieveName() {
+        return realName;
+    }
+
+    // Setters
+
+    public void updateName(String name) {
+        this.realName = name;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.userName = username;
+    }
     @Override
     public String toString() {
-        String receiversString = "";
-        if (!receivers.isEmpty()) {
-            receiversString = receivers.get(0).getUsername();
-            for (int i = 1; i < receivers.size(); i++) {
-                receiversString += ", " + receivers.get(i).getUsername();
-            }
+        String output = realName + ",";
+        for (int i = 0; i < friends.size(); i++) {
+            output = output + friends.get(i).getUsername() + ";";
+        }
+        output += ",";
+
+        for (int i = 0; i < blocked.size(); i++) {
+            System.out.println();
+            output = output + blocked.get(i).getUsername() + ";";
         }
 
-        return "Message{" +
-                "sender=" + sender.getUsername() +
-                ", receivers=[" + receiversString + "]" +
-                ", timestamp='" + timestamp + '\'' +
-                ", exactTime=" + exactTime +
-                ", content='" + content + '\'' +
-                '}';
+        output = output + "," + password + "," + emailAddress + "," + userName;
+
+        return  output;
     }
 }
