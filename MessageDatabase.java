f67e17be3bf3ad3823c2e2976a9ad441665f295e
efaruqi <eeshafaruqi@gmail.com>
2024-04-14 22:17:21 -0400

    adding delete message

--- MessageDatabase.java
+++ MessageDatabase.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.io.*;
+import java.util.Collections;
+
 /**
  * MessageDatabase
  *
@@ -35,6 +37,51 @@ public class MessageDatabase implements MessageData {
             return false;
         }
     }
+    public boolean deleteMessage(String fileName, Message m) throws FileNotFoundException, IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            ArrayList<Message> messages= new ArrayList<>();
+            String line = reader.readLine();
+            while (line != null) {
+                // read next line
+                int countCommas = 0;
+                int messageIndex = 0;
+                for(int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == ',') {
+                        countCommas++;
+                    }
+                    if (countCommas == 4) {
+                        messageIndex = i;
+                    }
+                }
+                String message = line.substring(messageIndex + 1);
+                String data = line.substring(0,messageIndex);
+                String[] dataParts = data.split(",");
+                String[] receiversArr = dataParts[1].split(";");
+                ArrayList<String> receivers = new ArrayList<>();
+                Collections.addAll(receivers, receiversArr);
+                Message readMessage = new Message(dataParts[0],receivers,message);
+                messages.add(readMessage);
+                line = reader.readLine();
+            }
+            // go through each line in the file and make a new message object for each line
+            // while loop going throuh file:
+            // while end
+            // go through array list of messages and check if sameDM to your parameter m
+            // if you find it remove and return true
+
+            for (int i = 0; i < messages.size(); i++) {
+                if (m.sameDM(messages.get(i))) {
+                    messages.remove(messages.get(i));
+                }
+            }
+            return false;
+        } catch (IOException e) {
+            return false;
+        }
+
+
+    }
+
 
     public ArrayList<String> getChats(String username) {
         ArrayList<String> chats = new ArrayList<>();
