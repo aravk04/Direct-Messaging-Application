25e5f86c4b7e097236cac65e8a602b6ade111edc
efaruqi <164688377+efaruqi@users.noreply.github.com>
2024-04-15 12:49:46 -0400

    Update ClientInterface.java

--- ClientInterface.java
+++ ClientInterface.java
@@ -1,3 +1,17 @@
+/**
+ * An interface of data
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Harshil, Zonglin, Arav, Eesha, Benjamin
+ * @version April 1st, 2024
+ */
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public interface ClientInterface {
     public void run();
 }
