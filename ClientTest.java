366ddbe9e5a6ad0c1179031fff724ef4f72fc1ad
aravk04 <143557911+aravk04@users.noreply.github.com>
2024-04-29 10:43:18 -0400

    Update ClientTest.java

--- ClientTest.java
+++ ClientTest.java
@@ -1,6 +1,13 @@
 import java.io.IOException;
 import java.net.UnknownHostException;
-
+/**
+ * ClientTest
+ *
+ * client testing
+ *
+ * @author Harshil, Zonglin, Arav, Eesha, Benjamin
+ * @version April 1st, 2024
+ */
 public class ClientTest {
     public static void main(String[] args) throws UnknownHostException,
             IOException, ClassNotFoundException, InterruptedException {
