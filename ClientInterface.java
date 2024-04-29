708596b0ce3602abe7344d81a01726f87677649a
aravk04 <143557911+aravk04@users.noreply.github.com>
2024-04-29 10:42:20 -0400

    Update ClientInterface.java

--- ClientInterface.java
+++ ClientInterface.java
@@ -1,17 +1,15 @@
-/**
- * An interface of data
- *
- * <p>Purdue University -- CS18000 -- Spring 2024</p>
- *
- * @author Harshil, Zonglin, Arav, Eesha, Benjamin
- * @version April 1st, 2024
- */
-
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+/**
+ * An interface for client
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Harshil, Zonglin, Arav, Eesha, Benjamin
+ * @version April 29th, 2024
+ */
 public interface ClientInterface {
     public void run();
 }
